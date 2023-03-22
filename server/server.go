7d2d5010f95f5046b5ef4a8183de176f6fdbcd32
kb <71296367+kwakubiney@users.noreply.github.com>
2023-03-22 14:44:32 +0000

    chore: setup model db migration and server (#1)
    * chore: setup model db migration and server
    
    * chore: remove TODO
    
    * fix: removed idea folders

---
+++ server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"fmt"
+	"fx-bank/middlewares"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	e   *gin.Engine
+	srv http.Server
+}
+
+func New() *Server {
+	return &Server{
+		e: gin.Default(),
+	}
+}
+
+func (s *Server) SetupMiddlewares(m []gin.HandlerFunc) {
+	s.e.Use(m...)
+}
+
+func (s *Server) SetupRoutes() *gin.Engine {
+	mw := []gin.HandlerFunc{middlewares.Cors()}
+	s.SetupMiddlewares(mw)
+	return s.e
+}
+
+func (s *Server) Start() {
+
+	s.srv = http.Server{
+		Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
+		Handler: s.SetupRoutes(),
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		if err := s.srv.Close(); err != nil {
+			log.Println("failed to shutdown server", err)
+		}
+	}()
+
+	if err := s.srv.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("server closed after interruption")
+		} else {
+			log.Println("unexpected server shutdown. err:", err)
+		}
+	}
+}
+
+func (s *Server) Stop() error {
+	return s.srv.Close()
+}
